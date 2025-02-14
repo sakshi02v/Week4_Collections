e4b1e61e4bd742483684b5ea7cb1cc5d688f2f34
Sakshi Verma <sakshi02v@gmail.com>
2025-02-14 22:26:18 +0530

    removed

--- Day_1/src/main/java/org/example/personalizedmealplan/MealPlanGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.example.personalizedmealplan;
-
-class MealPlanGenerator {
-    public static <T extends MealPlan> void validateAndGenerateMealPlan(T mealPlan) {
-        // Validation logic can be added here
-        System.out.println("Validating meal plan...");
-        mealPlan.displayPlan();
-    }
-}

f50cbe1612652fa527ddef3ec0ab34d6c252c164
Sakshi Verma <sakshi02v@gmail.com>
2025-02-15 22:03:25 +0530

    [Sakshi Verma]Testing parametrized test for even number

---
+++ Junit/src/main/java/org/example/parametrizedtest/Number.java
@@ -0,0 +1,10 @@
+package org.example.parametrizedtest;
+
+public class Number {
+
+        // Method to check if a number is even
+        public static boolean isEven(int number) {
+            return number % 2 == 0;
+        }
+    }
+
---
+++ Junit/src/test/java/org/example/parametrizedtest/NumberTest.java
@@ -0,0 +1,21 @@
+package org.example.parametrizedtest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NumberTest {
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10})  // Even numbers
+    void testIsEven_WithEvenNumbers(int number) {
+        assertTrue(Number.isEven(number), number + " should be even");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7, 9})  // Odd numbers
+    void testIsEven_WithOddNumbers(int number) {
+        assertFalse(Number.isEven(number), number + " should be odd");
+    }
+}
+
